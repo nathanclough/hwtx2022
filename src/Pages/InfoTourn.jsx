6ce31cd08952fe12742574f153b07e091fe73e54
Joseph-McClaine <joseph.mcclaine@ttu.edu>
2022-02-27 00:16:01 -0600

    fix errors on Create Tournament

--- src/Pages/InfoTourn.jsx
+++ src/Pages/InfoTourn.jsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 
 
-export default function CustomizedInputBase() {
+export default function CustomizedInputBase(props) {
   return (
     <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'space-between', height: '90%', paddingTop:'10px' }}>
       <Typography variant="h5" align='center' color = 'black' fontWeight={'bold'}>
       Tournament Name:
+      </Typography>
+      {props.name}
+      <Typography variant="h5" align='center' color = 'black' fontWeight={'bold'}>
+      Game Choice:
       </Typography>
     </div>
   );
